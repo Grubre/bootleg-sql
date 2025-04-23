014c623d8368f1888913d2a539d8dc7cc9f4a9e0
Grubre <ogrodowczykjakub@gmail.com>
2025-04-23 13:47:49 +0200

    Grammar: Created a base for what will eventually become the SQL SELECT statement in the grammar.

--- src/grammar/Grammar.g4
+++ src/grammar/Grammar.g4
@@ -1,20 +1,58 @@
+// based on diagrams from https://www.sqlite.org/lang.html
 grammar Grammar;
-LEFT_PARENTHESIS: '(';
-RIGHT_PARENTHESIS: ')';
-CARET: '^';
-STAR: '*';
-SLASH: '/';
-PLUS: '+';
-MINUS: '-';
-NUMBER: [0-9]+;
-WHITESPACE: [ \r\n\t]+ -> skip;
 
-start : expression;
-expression
-   : NUMBER                                                 # Number
-   | MINUS right=expression                                 # Negation
-   | LEFT_PARENTHESIS inner=expression RIGHT_PARENTHESIS    # Parentheses
-   | left=expression operator=CARET right=expression        # Power
-   | left=expression operator=(STAR|SLASH) right=expression # MultiplicationOrDivision
-   | left=expression operator=(PLUS|MINUS) right=expression # AdditionOrSubtraction
-   ;
+program : sql_stmt+;
+
+sql_stmt
+    : select_stmt
+    ;
+
+// https://www.sqlite.org/lang_select.html
+select_stmt
+    : SELECT (DISTINCT | ALL)? result_column (COMMA result_column)* FROM table_or_subquery (COMMA table_or_subquery)*
+    ;
+
+result_column
+    : expr (AS column_alias)?
+    | STAR
+    | table_name DOT STAR
+    ;
+
+table_or_subquery
+    : table_name
+    ;
+
+expr
+    : IDENTIFIER
+    ;
+
+column_alias
+    : IDENTIFIER
+    ;
+
+table_name
+    : IDENTIFIER
+    ;
+
+column_name
+    : IDENTIFIER
+    ;
+
+fragment DIGIT      : [0-9]     ;
+fragment LETTER     : [a-zA-Z]  ;
+fragment ID_CHAR    : (LETTER | DIGIT | '_' | '$') ;
+
+IDENTIFIER
+    : LETTER ID_CHAR*
+    ;
+
+SELECT : 'SELECT';
+DISTINCT : 'DISTINCT';
+ALL : 'ALL';
+AS : 'AS';
+
+COMMA : ',';
+DOT : '.';
+STAR : '*';
+
+WHITESPACE: [ \r\n\t]+ -> skip;
--- src/main.cpp
+++ src/main.cpp
@@ -1,78 +1,106 @@
 #include <cmath>
 #include <fmt/base.h>
+#include <fmt/format.h>
+#include "expected.hpp"
 
 #include "GrammarLexer.h"
 #include "GrammarParser.h"
 #include "GrammarBaseVisitor.h"
 
-class CalculatorInterpreter : public GrammarBaseVisitor {
+enum class Opcode {
+    NOOP,
+    HALT
+};
+enum class P4type {
+    I32,
+    I64,
+    F64,
+    STR,
+    BLOB
+};
+struct Instruction {
+    Opcode opcode;
+    std::int32_t P1;
+    std::int32_t P2;
+    std::int32_t P3;
+    void* P4;
+    P4type p4type;
+    std::uint16_t P5;
+};
+
+using SqlBytecodeProgram = std::vector<Instruction>;
+using ProgramOutput = std::vector<SqlBytecodeProgram>;
+
+struct SqlError : std::runtime_error {
+    using std::runtime_error::runtime_error;
+};
+
+template <typename... Args>
+[[noreturn]] void fail(fmt::format_string<Args...> fmtstr, Args&&... args) {
+    throw SqlError{fmt::format(fmtstr, std::forward<Args>(args)...)};
+}
+
+enum class SelectModifier {
+    NONE,
+    DISTINCT,
+    ALL
+};
+
+class SqlGrammarVisitor : public GrammarBaseVisitor {
 public:
-    std::any visitStart(GrammarParser::StartContext *ctx) override {
-        try {
-            return visit(ctx->expression());
-        } catch (const std::exception& e) {
-            fmt::println("Runtime error: {}", e.what());
-            return std::numeric_limits<double>::quiet_NaN();
+    std::any visitProgram(GrammarParser::ProgramContext *ctx) override {
+        auto programs = std::vector<SqlBytecodeProgram>(ctx->children.size());
+
+        for (auto i = 0u; i < ctx->children.size(); i++) {
+            programs[i] = std::any_cast<SqlBytecodeProgram>(visit(ctx->children[i]));
         }
+
+        return programs;
     }
 
-    std::any visitNumber(GrammarParser::NumberContext *ctx) override {
-        try {
-            return std::stod(ctx->NUMBER()->getText());
-        } catch (const std::out_of_range&) {
-            fmt::println("Error: Number out of range: {}", ctx->NUMBER()->getText());
-            return std::numeric_limits<double>::quiet_NaN();
-        } catch (const std::invalid_argument &) {
-            fmt::println("Error: Invalid number format: {}", ctx->NUMBER()->getText());
-            return std::numeric_limits<double>::quiet_NaN();
+    std::any visitSql_stmt(GrammarParser::Sql_stmtContext *ctx) override {
+        if (ctx->select_stmt()) {
+            return visit(ctx->select_stmt());
         }
+        fail("Invalid SQL statement '{}'", ctx->toString());
     }
 
-    std::any visitNegation(GrammarParser::NegationContext *ctx) override {
-        double right = std::any_cast<double>(visit(ctx->right));
-        return -right;
+    std::any visitSelect_stmt(GrammarParser::Select_stmtContext *ctx) override {
+        const auto modifier = ctx->ALL() ? SelectModifier::ALL : (ctx->DISTINCT() ? SelectModifier::DISTINCT : SelectModifier::NONE);
+        return nullptr;
     }
 
-    std::any visitParentheses(GrammarParser::ParenthesesContext *ctx) override {
-        return visit(ctx->inner); // Evaluate the inner expression
-    }
-
-    std::any visitPower(GrammarParser::PowerContext *ctx) override {
-        double left = std::any_cast<double>(visit(ctx->left));
-        double right = std::any_cast<double>(visit(ctx->right));
-        return std::pow(left, right);
-    }
-
-    std::any visitMultiplicationOrDivision(GrammarParser::MultiplicationOrDivisionContext *ctx) override {
-        double left = std::any_cast<double>(visit(ctx->left));
-        double right = std::any_cast<double>(visit(ctx->right));
-
-        if (ctx->operator_->getType() == GrammarParser::STAR) {
-            return left * right;
-        } else { // SLASH
-            if (right == 0.0) {
-                std::cerr << "Error: Division by zero." << std::endl;
-                return std::numeric_limits<double>::quiet_NaN();
-            }
-            return left / right;
+    std::any visitResult_column(GrammarParser::Result_columnContext *ctx) override {
+        if (ctx->expr()) {
+            return visit(ctx->expr());
         }
+        fail("Invalid result column value");
     }
 
-    std::any visitAdditionOrSubtraction(GrammarParser::AdditionOrSubtractionContext *ctx) override {
-        double left = std::any_cast<double>(visit(ctx->left));
-        double right = std::any_cast<double>(visit(ctx->right));
+    std::any visitTable_or_subquery(GrammarParser::Table_or_subqueryContext *ctx) override {
+        return visit(ctx->table_name());
+    }
 
-        if (ctx->operator_->getType() == GrammarParser::PLUS) {
-            return left + right;
-        } else { // MINUS
-            return left - right;
-        }
+    std::any visitExpr(GrammarParser::ExprContext *ctx) override {
+        return ctx->IDENTIFIER();
+    }
+
+    std::any visitColumn_alias(GrammarParser::Column_aliasContext *ctx) override {
+        return ctx->IDENTIFIER();
+    }
+
+    std::any visitTable_name(GrammarParser::Table_nameContext *ctx) override {
+        return ctx->IDENTIFIER();
+    }
+
+    std::any visitColumn_name(GrammarParser::Column_nameContext *ctx) override {
+        return ctx->IDENTIFIER();
     }
 };
 
 int main() {
     std::string line;
-    CalculatorInterpreter interpreter;
+    SqlGrammarVisitor bytecode_generator;
 
     fmt::print("> ");
     while (std::getline(std::cin, line)) {
@@ -85,18 +113,12 @@ int main() {
         GrammarLexer lexer(&input);
         antlr4::CommonTokenStream tokens(&lexer);
         GrammarParser parser(&tokens);
-        antlr4::tree::ParseTree *tree = parser.start();
-        std::any result_any = interpreter.visit(tree);
-
+        antlr4::tree::ParseTree *tree = parser.program();
         try {
-            double result = std::any_cast<double>(result_any);
-            if (std::isnan(result)) {
-                fmt::println("Calculation resulted in an error.");
-            } else {
-                fmt::println("{}", result);
-            }
-        } catch(const std::bad_any_cast& e) {
-            fmt::println("Error processing result.");
+            auto result_any = bytecode_generator.visit(tree);
+            auto programs = std::any_cast<ProgramOutput>(result_any);
+        } catch(const SqlError& e) {
+            fmt::println(stderr, "{}", e.what());
         }
 
         fmt::print("> ");
